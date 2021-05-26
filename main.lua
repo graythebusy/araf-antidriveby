691a5f00c6a15180f796444f8b670d9e0ba8b10f
araf <74408185+arafbusy@users.noreply.github.com>
2021-05-26 18:31:27 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,16 +1,17 @@
 Citizen.CreateThread(function()
-	while true do
-		if IsPedInAnyVehicle(PlayerPedId(), false) then
-			if GetPedInVehicleSeat(GetVehiclePedIsIn(PlayerPedId(), false), -1) and GetCurrentPedWeapon(PlayerPedId(), false) then
-				TriggerEvent('chat:addMessage', {
-					color = {150, 0, 0},
-					multiline = true,
-					args = {"Hata", "Sürücü koltuğundayken ateş edemezsiniz!"}
-				})
-				SetCurrentPedWeapon(PlayerPedId(), "WEAPON_UNARMED")
-			end
-		end
-
-		Citizen.Wait(0)
-	end
+    while true do
+        if IsPedInAnyVehicle(PlayerPedId(), false) then
+            if GetPedInVehicleSeat(GetVehiclePedIsIn(PlayerPedId(), false), -1) and GetCurrentPedWeapon(PlayerPedId(), false) then
+                TriggerEvent('chat:addMessage', {
+                    color = {150, 0, 0},
+                    multiline = true,
+                    args = {"Hata", "Sürücü koltuğundayken ateş edemezsiniz!"}
+                })
+                SetCurrentPedWeapon(PlayerPedId(), "WEAPON_UNARMED")
+            end
+        else
+            Citizen.Wait(2000)
+        end
+        Citizen.Wait(10)
+    end
 end)
